b6e266b3ae5b4ac9a6a7d425c0097a24c53d40fc
Robert <roberthklink@gmail.com>
2022-12-05 19:22:08 +0100

    6 boiler

---
+++ src/days/day6.rs
@@ -0,0 +1,11 @@
+use std::fs::read_to_string;
+
+pub fn solve1() {
+    let all_string = read_to_string("src/days/input/6.txt")
+        .unwrap();
+}
+
+pub fn solve2() {
+    let all_string = read_to_string("src/days/input/6.txt")
+        .unwrap();
+}
