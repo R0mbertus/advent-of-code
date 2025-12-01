50905e20253861bc94b4a47ce2e71cd9b4198955
Robert Klink <roberthklink@gmail.com>
2025-12-01 18:20:56 +0100

    init: 2025

--- aoc/lib.rs
+++ aoc/lib.rs
@@ -1,26 +1,5 @@
 use aoc_runner_derive::aoc_lib;
 
 mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day10;
-mod day12;
-mod day9;
-mod day14;
-mod day11;
-mod day13;
-mod day20;
-mod day21;
-mod day22;
-mod day23;
-mod day24;
-mod day19;
-mod day18;
-mod day25;
 
 aoc_lib! { year = 2024 }
